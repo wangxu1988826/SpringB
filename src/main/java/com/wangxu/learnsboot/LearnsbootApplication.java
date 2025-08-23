849f0283825d4dcb380fc10d6e05db27bad84a5b
wangxu1988826 <wangxu1988826@gmail.com>
2025-08-23 19:46:05 +0800

    first commit

---
+++ src/main/java/com/wangxu/learnsboot/LearnsbootApplication.java
@@ -0,0 +1,14 @@
+package com.wangxu.learnsboot;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplicationl
+public class LearnsbootApplication {
+    int a;
+
+    public static void main(String[] args) {
+        SpringApplication.run(LearnsbootApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/wangxu/learnsboot/LearnsbootApplicationTests.java
@@ -0,0 +1,13 @@
+package com.wangxu.learnsboot;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class LearnsbootApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
