a869e7fc5b3b2a01d4b5c4c9bfefe38b0552b1bc
wangxu1988826 <wangxu1988826@gmail.com>
2025-08-23 19:53:34 +0800

    add application

--- src/main/java/com/wangxu/learnsboot/LearnsbootApplication.java
+++ src/main/java/com/wangxu/learnsboot/LearnsbootApplication.java
@@ -3,9 +3,8 @@ package com.wangxu.learnsboot;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
-@SpringBootApplicationl
+@SpringBootApplication
 public class LearnsbootApplication {
-    int a;
 
     public static void main(String[] args) {
         SpringApplication.run(LearnsbootApplication.class, args);
